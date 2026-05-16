a7fe27cbc66c93553d6a46f73d3555a93ac960e4
Jesse Usanga <jesseusanga1@gmail.com>
2026-05-16 08:59:48 +0100

    added responsive pages and scroll behaviour

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns =[
     path("",views.home),
     path("agriculture/", views.agriculture, name="agriculture"),
-    path("engineering/", views.engineering, name="engineering")
+    path("engineering/", views.engineering, name="engineering"),
+    path("computing/",views.computing,name="computing"),
+    path("nutrition/",views.nutrition,name="nutrition"),
+    path("agribusiness/",views.agribusiness,name="agribusiness")
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,12 @@ def agriculture(request):
     return render(request,'agriculture.html')
 def engineering(request):
     return render(request,'engineering.html')
+
+def computing(request):
+    return render(request,'computing.html')
+
+def nutrition(request):
+    return render(request,'nutrition.html')
+
+def agribusiness(request):
+    return render(request,'agribusiness.html')
